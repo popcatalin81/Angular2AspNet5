d82574d094d34e0fbc33bb36b860f33f2809617d
popcatalin <pop.catalin@gmail.com>
2016-03-17 15:15:20 +0200

    Full SPA

--- src/Angular2/gulpfile.js
+++ src/Angular2/gulpfile.js
@@ -5,11 +5,13 @@ Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
 */
 var gulp = require("gulp");
 var rimraf = require("rimraf");
+var merge = require('merge-stream');
 
 var paths = {
     npm: "./node_modules/",
     lib: "./wwwroot/lib/",
-    dts: "./typings/"
+    dts: "./typings/",
+    assets: "./wwwroot/assets/"
 }
 
 var libs = [
@@ -20,11 +22,21 @@ var libs = [
 	paths.npm + "es6-shim/es6-shim.js",
 	paths.npm + "systemjs/dist/system.js",
 	paths.npm + "rxjs/bundles/rx.js",
-	paths.npm + "systemjs/dist/system-polyfills.js"
+	paths.npm + "systemjs/dist/system-polyfills.js",
+	paths.npm + "a2-in-memory-web-api/web-api.js",
+	paths.npm + "typescript/lib/typescript.js",
+	paths.npm + "angular2/ts/src/testing/shims_for_IE.js",
+	paths.npm + "material-design-lite/dist/material.min.js"
+];
+
+var assets = [
+    paths.npm + "material-design-lite/dist/material.deep_orange-pink.min.css"
 ];
 
 gulp.task("libs", function () {
-    return gulp.src(libs).pipe(gulp.dest(paths.lib));
+    var libTarget = gulp.src(libs).pipe(gulp.dest(paths.lib));
+    var assetTarget = gulp.src(assets).pipe(gulp.dest(paths.assets));
+    return merge(libTarget, assetTarget);
 });
 
 gulp.task("clean", function (callback) {
---
+++ src/Angular2/wwwroot/app/main.ts
@@ -0,0 +1,5 @@
+import { bootstrap } from 'angular2/platform/browser';
+import { AppComponent } from './app.component';
+bootstrap(AppComponent, [])
+  .then(success => console.log(`Bootstrap success`))
+  .catch(error => console.log(error));
